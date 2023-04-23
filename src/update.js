47c5696f11a7e32762e651e3c3e114ee0362a6a4
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2023-04-23 11:07:54 +0200

    fix: scripts & inputs

--- src/update.js
+++ src/update.js
@@ -2,12 +2,9 @@ const Handlebars = require('handlebars');
 const fs = require('fs');
 const GhostContentAPI = require('@tryghost/content-api');
 
-module.exports = async({ core }) => {
-    const templateContent = fs.readFileSync('./src/template.md');
+module.exports = async ({ghostUrl, apiKey}) => {
+    const templateContent = fs.readFileSync('./src/template.md', {encoding: 'utf-8'});
     const template = Handlebars.compile(templateContent);
-
-    const ghostUrl = core.getInput('ghost-url');
-    const apiKey = core.getInput('ghost-api-key');
 
     const api = new GhostContentAPI({
         url: ghostUrl,
@@ -15,13 +12,13 @@ module.exports = async({ core }) => {
         version: 'v5.0'
     });
 
-    const latestPosts = await api.posts.browse({ limit: 5, order: 'published_at DESC' });
+    const latestPosts = await api.posts.browse({limit: 5, order: 'published_at DESC'});
     const templateVariables = {
         lastUpdate: new Date().toISOString().split('T')[0],
-        posts: latestPosts.map(post => ({ title: post.title, url: post.url }))
+        posts: latestPosts.map(post => ({title: post.title, url: post.url}))
     }
 
     const finalTemplate = template(templateVariables);
 
-    fs.writeFileSync('./template.md', finalTemplate, { encoding: 'utf-8' });
+    fs.writeFileSync('./template.md', finalTemplate, {encoding: 'utf-8'});
 }
